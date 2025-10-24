fbc4a33a2e0fb510d7c172a80c7dc7d1fe4de493
Rosales <140207733+erosales83@users.noreply.github.com>
2025-10-24 12:20:42 -0700

    Added KeyPresent

--- carnet.py
+++ carnet.py
@@ -8,6 +8,7 @@ car_model = DiscreteBayesianNetwork(
         ("Battery", "Ignition"),
         ("Ignition","Starts"),
         ("Gas","Starts"),
+        ("KeyPresent", "Starts"),
         ("Starts","Moves"),
 ])
 
@@ -42,13 +43,20 @@ cpd_ignition = TabularCPD(
                  "Battery": ['Works',"Doesn't work"]}
 )
 
+cpd_key = TabularCPD(
+    variable= "KeyPresent", variable_card=2,
+    values= [[0.7],[0.3]],
+    state_names={"KeyPresent": ["yes", "no"]},
+
+)
+
 cpd_starts = TabularCPD(
     variable="Starts",
     variable_card=2,
     values=[[0.95, 0.05, 0.05, 0.001], [0.05, 0.95, 0.95, 0.9999]],
-    evidence=["Ignition", "Gas"],
-    evidence_card=[2, 2],
-    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"]},
+    evidence=["Ignition", "Gas", "KeyPresent"],
+    evidence_card=[2, 2, 2],
+    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"], "KeyPresent": ["yes", "no"]},
 )
 
 cpd_moves = TabularCPD(
@@ -62,14 +70,15 @@ cpd_moves = TabularCPD(
 
 
 # Associating the parameters with the model structure
-car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves)
+car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves, cpd_key)
 
 car_infer = VariableElimination(car_model)
 
 # print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
 
 def main():
-
+    query_key = car_infer.query(variables=["KeyPresent"], evidence={"Moves": "no"})
+    print(query_key)
     query_one = car_infer.query(variables= ["Battery"], evidence={"Moves": "no"})
     print(query_one)
     query_two = car_infer.query(variables=["Starts"], evidence={"Radio": "Doesn't turn on"})
