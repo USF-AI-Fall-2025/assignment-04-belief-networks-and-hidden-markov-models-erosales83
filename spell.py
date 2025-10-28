3fc12ff3d9b5bd0cf7177284dd195176e3559a2f
Rosales <140207733+erosales83@users.noreply.github.com>
2025-10-28 12:05:59 -0700

    updated load_data method and starting emission method

--- spell.py
+++ spell.py
@@ -6,8 +6,10 @@ def load_data(file = "aspell.txt"):
         for line in f:
             if ":" not in line:
                 continue
-            correct, wrongs = line.strip().split()
-            correct = correct.strip()
+            correct, wrongs = line.strip().split(":", 1)
+            correct = correct.strip().lower()
             for wrong in wrongs.strip().split():
-                data.append((correct, wrong))
-    return data
+                data.append((correct, wrong.lower()))
+        return data
+
+def emission_probabilities(data):
