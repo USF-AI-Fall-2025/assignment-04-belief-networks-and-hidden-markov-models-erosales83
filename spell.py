4ab606e1e5328db6097c9b0e8c0c308dc7229108
Rosales <140207733+erosales83@users.noreply.github.com>
2025-10-28 19:50:55 -0700

    progress, implemented emission_probs

--- spell.py
+++ spell.py
@@ -1,7 +1,7 @@
 import math
 import string
 
-def load_data(file="aspell.txt"):
+def load_data(file = "aspell.txt"):
     data = []
     with open(file) as f:
         for line in f:
@@ -15,22 +15,52 @@ def load_data(file="aspell.txt"):
 
 def emission_probs(data):
     em_counts = {}
+    for correct, typed in data:
+        for c, t in zip(correct, typed):
+            em_counts.setdefault(c, {})
+            em_counts[c][t] = em_counts[c].get(t, 0) + 1
+
+    # Testing Purposes
+    # print("Emission counts:")
+    # for c, t_counts in em_counts.items():
+    #     print(f"{c}: {t_counts}")
+
+    em_probs = {}
+    for c, t_counts in em_counts.items():
+        total = sum(t_counts.values())
+        em_probs[c] = {}
+        for t, v in t_counts.items():
+            em_probs[c][t] = v / total
+
+    # Testing Purposes
+    # print("Emission probabilities:")
+    # for c, t_counts in em_probs.items():
+    #     print(f"{c}: {t_counts}")
+    return em_probs
+
 
 def transition_probs(data):
     tr_counts = {}
-
+    tr_probs = {}
+    return tr_probs
 
 def viterbi(word, states, start_probs, trans_probs, emit_probs):
     V = [{}]
     path = {}
+    return correct_word
 
 
 def main():
     data = load_data("aspell.txt")
     em_probs = emission_probs(data)
     tr_probs = transition_probs(data)
-    start_probs =
-    states =
+    start_counts = tr_probs.get("<s>", {})
+    total_starts = sum(start_counts.values())
+    start_probs = {}
+    for k, v in start_counts.items():
+        probs = v / total_starts
+        start_probs[k] = probs
+    states = set(string.ascii_lowercase)
     while True:
         user_input = input("Please enter a word to decode ('quit' to exit): ").strip().lower()
         if user_input == 'quit':
